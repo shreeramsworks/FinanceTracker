840b72e9c29acd915071f6382bce5dbe3805ccae
shreeram kulkarni <kulkarni.shreerams.mails@gmail.com>
2025-02-28 13:52:03 +0530

    Add files via upload

---
+++ components/ui/use-toast.ts
@@ -0,0 +1,6 @@
+// Adapted from https://ui.shadcn.com/docs/components/toast
+import { useToast as useToastOriginal } from "@/components/ui/toast"
+
+export const useToast = useToastOriginal
+export const toast = useToastOriginal
+
